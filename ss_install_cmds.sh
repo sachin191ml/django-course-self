40f988160ef3afda48879eb171dc91bfa72b798a
Sac ML <sachin191ml@gmail.com>
2025-03-30 10:02:38 -0700

    [SS] Updated instruction to activate/deacivate py env

--- ss_install_cmds.sh
+++ ss_install_cmds.sh
@@ -1,6 +1,9 @@
 python.exe -m pip install --upgrade pip
 py -m venv .venv
+source .venv/Scripts/activate
 py -m pip install Django
 django-admin startproject myproject
 cd myproject
 py manage.py runserver
+deactivate
+dos2unix README.md
