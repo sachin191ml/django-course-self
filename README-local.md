8f86c7a3426130793c0eba54a7ac6eef85ee3bf6
Sac ML <sachin191ml@gmail.com>
2025-03-31 09:35:30 -0700

    [SS] Updated my shell script and added README-local file

---
+++ README-local.md
@@ -0,0 +1,3 @@
+#  Djano - Learning
+* Tutorial: https://www.youtube.com/watch?v=Rp5vd34d-z4
+* https://github.com/gitdagray/django-course
--- ss_install_cmds.sh
+++ ss_install_cmds.sh
@@ -1,9 +1,20 @@
+# Tutorial: https://www.youtube.com/watch?v=Rp5vd34d-z4
+# Install python with Djano extension
 python.exe -m pip install --upgrade pip
 py -m venv .venv
 source .venv/Scripts/activate
 py -m pip install Django
+
+# Start a new django project
 django-admin startproject myproject
 cd myproject
 py manage.py runserver
-deactivate
+
+# Create a new app inside a project
+cd myproject
+py manage.py startapp posts
+
+# Misc commands
 dos2unix README.md
+
+deactivate
